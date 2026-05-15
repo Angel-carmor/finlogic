3d92adfd908bc319f1d6bc9236b4a27f9f092e74
Angel-carmor <angel.cardenas.moraa@gmail.com>
2026-05-15 19:50:28 +0200

    railway

--- api/src/index.js
+++ api/src/index.js
@@ -4,7 +4,10 @@ const pool = require('./config/db');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://finlogic-l7cqwyck2-angels-projects-525d4ecd.vercel.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 const authRoutes = require('./routes/auth.routes');
